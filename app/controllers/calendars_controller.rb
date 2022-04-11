4b847a7ac4ac838115067603a6fd4fd11bef6ed8
near7830 <near7830@gmail.com>
2022-04-11 18:52:49 +0900

    修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,7 +36,7 @@ class CalendarsController < ApplicationController
       end
 
 
-      wday_num = x + 1
+      wday_num = Date.today.wday
       if wday_num >= 7
         wday_num = wday_num -7
       end
